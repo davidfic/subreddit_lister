ecc16a8ccbbc83aefc8f06a00da457946bbbd207
David Ficociello <davidfic@gmail.com>
2017-04-26 16:05:51 -0400

    added ability to look at multiple users at once

--- list_subreddits.py
+++ list_subreddits.py
@@ -1,19 +1,34 @@
 import praw
 import click
-from config import client_id, client_secret, user_agent,user_info 
+from config import user_agent,user_info 
+
+user_dict = {}
+sub_list = []
+def get_subreddits(user):
+    u = user_info[user]
+    reddit = praw.Reddit(client_id=u['client_id'], client_secret=u['client_secret'], user_agent=user_agent,username=user, password=u['password'])
+    sub_list = list(reddit.user.subreddits(limit=None))
+    return sub_list 
 
 
 @click.command()
-@click.option('--user', help='the user to pull subreddits from')
-def make_list(user):
-    if user in user_info: 
-        u = user_info[user]
-        reddit = praw.Reddit(client_id=u['client_id'], client_secret=u['client_secret'], user_agent=user_agent,username=user, password=u['password'])
-    subscribed = list(reddit.user.subreddits(limit=None))
+@click.option('--user', help='the user to pull subreddits from', required=False)
+@click.option('--user_list', help='a comma separated list of users to lookup', required=False)
+def get_list(user,user_list=None):
 
-    for sub in subscribed:
-        print(sub)
+    u_list = user_list.split(',')
+    for user in u_list:
+        user_dict[user] = get_subreddits(user)
+                 
 
+    for k,v in user_dict.iteritems():
+        print("user is {}".format(k))
+        print("subreddits are as follows")
+        for value in v:
+            print(value)
+
+def compare_list(user_list):
+    pass
 
 if __name__ == '__main__':
-    make_list()
+    get_list()
