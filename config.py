5125d5a79da4efd434b2f688cedfca479ffcce14
David Ficociello <davidfic@gmail.com>
2017-04-26 10:31:51 -0400

    initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+user_agent='my user agent'
+user_info={
+        'USERNAME': {'password': 'PASSWORD','client_id': 'CLIENT_ID', 'client_secret': 'CLIENT_SECRET'},
+        'USERNAME': {'password': 'PASSWORD','client_id': 'CLIENT_ID', 'client_secret': 'CLIENT_SECRET'}
+        }
+
+
---
+++ list_subreddits.py
@@ -0,0 +1,20 @@
+import praw
+import click
+from config import client_id, client_secret, user_agent,user_info 
+
+
+
+@click.command()
+@click.option('--user', help='the user to pull subreddits from')
+def make_list(user):
+    if user in user_info: 
+        u = user_info[user]
+        reddit = praw.Reddit(client_id=u['client_id'], client_secret=u['client_secret'], user_agent=user_agent,username=user, password=u['password'])
+    subscribed = list(reddit.user.subreddits(limit=None))
+
+    for sub in subscribed:
+        print(sub)
+
+
+if __name__ == '__main__':
+    make_list()
